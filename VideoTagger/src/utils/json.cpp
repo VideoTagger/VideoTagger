eb65b519a47d9bb1595cd16123bc2bd46466adef
Gracjan Chojnacki <cg50944>
2023-12-04 23:52:26 +0100

    Adding json

--- VideoTagger/src/utils/json.cpp
+++ VideoTagger/src/utils/json.cpp
@@ -1,13 +1,24 @@
 #include "json.hpp"
+#include <fstream>
 namespace vt::utils::json
 {
 	nlohmann::json load_from_file(const std::filesystem::path& filepath)
 	{
-		nlohmann::json b;
-		return b;
+		std::ifstream file(filepath);
+		if (!file.is_open()) {
+			throw std::runtime_error("Filed opening file due to reading");
+		}
+		nlohmann::json jsonData;
+		file >> jsonData;
+
+		return jsonData;;
 	}
 	void write_to_file(const nlohmann::json& data, const std::filesystem::path& filepath)
 	{
+		std::ofstream file(filepath);
+		if (!file.is_open()) {
+			throw std::runtime_error("Filed opening file due to reading");
+		}
+		file << std::setw(4) << data;
 	}
 }
-
