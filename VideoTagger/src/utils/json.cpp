f7f358ebe46a175ad19e6c5c31ecb381dab69b2a
Gracjan Chojnacki <cg50944>
2023-12-06 10:19:12 +0100

    Json serialization and deserialization

--- VideoTagger/src/utils/json.cpp
+++ VideoTagger/src/utils/json.cpp
@@ -1,11 +1,13 @@
 #include "json.hpp"
+#include "iostream"
 #include <fstream>
 namespace vt::utils::json
 {
 	nlohmann::json load_from_file(const std::filesystem::path& filepath)
 	{
 		std::ifstream file(filepath);
-		if (!file.is_open()) {
+		if (!file.is_open())
+		{
 			throw std::runtime_error("Filed opening file due to reading");
 		}
 		nlohmann::json jsonData;
@@ -16,9 +18,11 @@ namespace vt::utils::json
 	void write_to_file(const nlohmann::json& data, const std::filesystem::path& filepath)
 	{
 		std::ofstream file(filepath);
-		if (!file.is_open()) {
+		if (!file.is_open())
+		{
 			throw std::runtime_error("Filed opening file due to reading");
 		}
 		file << std::setw(4) << data;
 	}
 }
+
