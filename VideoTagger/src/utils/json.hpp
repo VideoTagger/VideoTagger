3baa74e4fbc1eed4e2eaaab4a8d66c0ac8707e30
ScriptExec <scriptexec.contact@gmail.com>
2024-01-03 11:08:26 +0100

    Json improvements, project system fix and window size serialization

--- VideoTagger/src/utils/json.hpp
+++ VideoTagger/src/utils/json.hpp
@@ -5,5 +5,5 @@
 namespace vt::utils::json
 {
 	nlohmann::ordered_json load_from_file(const std::filesystem::path& filepath);
-	void write_to_file(const nlohmann::ordered_json& data, const std::filesystem::path& filepath);
+	void write_to_file(const nlohmann::ordered_json& data, const std::filesystem::path& filepath, bool compact = false);
 }
