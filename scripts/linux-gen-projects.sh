3f9acedb9b493a12dd170087b9f037c7b9547208
ScriptExec <scriptexec.contact@gmail.com>
2024-03-26 19:09:07 +0100

    Minor build script tweak

--- scripts/linux-gen-projects.sh
+++ scripts/linux-gen-projects.sh
@@ -9,4 +9,5 @@ fi
 cd $(dirname $0)/../
 chmod +x tools/premake/linux/premake5
 ./tools/premake/linux/premake5 gmake2
-read -rsn1 -p "Press any key to continue . . . "
+read -rsn1 -p "Press any key to continue . . ."
+echo -e
--- scripts/macos-gen-projects.sh
+++ scripts/macos-gen-projects.sh
@@ -9,4 +9,5 @@ fi
 cd $(dirname $0)/../
 chmod +x tools/premake/macos/premake5
 ./tools/premake/macos/premake5 xcode4
-read -rsn1 -p "Press any key to continue . . . "
+read -rsn1 -p "Press any key to continue . . ."
+echo -e
