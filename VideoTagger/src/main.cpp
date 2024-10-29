003cf42f8bb9047f397e6bc65c4ddc677ebb0d9b
Vexte <60191743+Vexte@users.noreply.github.com>
2024-10-29 21:50:17 +0100

    Removed testing code from main
    d

--- VideoTagger/src/main.cpp
+++ VideoTagger/src/main.cpp
@@ -6,14 +6,6 @@
 
 int main(int argc, char* argv[])
 {
-	auto git_path = vt::git::get_global_git_path();
-	fmt::println("{}", git_path.u8string());
-
-	vt::git::git_wrapper lol(git_path, "./");
-	auto command_result = lol.execute_command("commit", {"-m", "Fixed command execution\nCommited using my git wrapper", "-a"});
-
-	fmt::println("{}", *command_result.return_value());
-
 	vt::app app;
 	vt::app_config cfg;
 	cfg.window_pos_x = -1;
