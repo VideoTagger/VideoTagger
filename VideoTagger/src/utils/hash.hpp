bdd078b81a202e34348e529befbab15f33f51649
Vexte <60191743+Vexte@users.noreply.github.com>
2024-11-17 17:05:20 +0100

    SHA256 from a file, hash string conversion

--- VideoTagger/src/utils/hash.hpp
+++ VideoTagger/src/utils/hash.hpp
@@ -9,4 +9,14 @@ namespace vt::utils::hash
 	extern uint64_t fnv_hash(const std::filesystem::path& filepath);
 
 	extern std::vector<uint8_t> sha256(std::string_view string);
+	extern std::vector<uint8_t> sha256_file(const std::filesystem::path& filepath);
+
+	enum class string_case
+	{
+		upper,
+		lower
+	};
+
+	extern std::string bytes_to_hex(const std::vector<uint8_t>& bytes, string_case result_case);
+	extern std::vector<uint8_t> hex_to_bytes(std::string_view hex_string);
 }
