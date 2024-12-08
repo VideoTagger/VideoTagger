5be36171fcd7403cb1f7883ec7cc39dbd83ed75b
ScriptExec <scriptexec.contact@gmail.com>
2024-12-08 18:54:00 +0100

    Python setup changes

--- VideoTagger/setup-python.lua
+++ VideoTagger/setup-python.lua
@@ -14,20 +14,34 @@ end
 
 function python_find_path()
 	local path = sys_invoke('python -c "import sys; import os; print(os.path.dirname(sys.executable))"')
-	
+
 	path = string.gsub(path, "\\\\", "\\")
 	path = string.gsub(path, "\\", "/")
 	return path
 end
 
-function python_get_lib_name()
-    return sys_invoke("python -c \"import sys; import os; import glob; path = os.path.dirname(sys.executable); libs = glob.glob(path + '/libs/python*'); print(os.path.splitext(os.path.basename(libs[-1]))[0]);\"")
+function python_get_include_path()
+	if os.get() == "windows" then
+		return python_find_path() .. "/include"
+	else
+		return sys_invoke('python -c "from sysconfig import get_paths as gp; print(gp()[\'include\'])"')
+	end
+end
+
+function python_get_lib_name_and_path()
+	if os.get() == "windows" then
+		local lib_name =  sys_invoke("python -c \"import sys; import os; import glob; path = os.path.dirname(sys.executable); libs = glob.glob(path + '/libs/python*'); print(os.path.splitext(os.path.basename(libs[-1]))[0]);\"")
+		return lib_name, python_find_path() .. "/libs"
+	else
+		local lib_name = sys_invoke('python -c "import sysconfig; print(sysconfig.get_config_var(\'LDLIBRARY\'))"')
+		local lib_path = sys_invoke('python -c "from sysconfig import get_config_var as gcv; print(gcv(\'LIBDIR\') or gcv(\'LIBPL\'))"')
+		return lib_name, lib_path
+	end
 end
 
 PythonPath = python_find_path()
-PythonIncludePath = PythonPath .. "/include"
-PythonLibPath = PythonPath .. "/libs"
-PythonLibName = python_get_lib_name()
+PythonIncludePath = python_get_include_path()
+PythonLibName, PythonLibPath = python_get_lib_name_and_path()
 
 if PythonPath == "" or PythonLibName == "" then
     error("Failed to find Python path")
