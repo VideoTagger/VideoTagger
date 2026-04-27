bfc5aeb87bf92bd01fc2589dcd1556da01fe8663
Vexte <60191743+Vexte@users.noreply.github.com>
2026-04-27 21:52:16 +0200

    Fixed simple attribute instance serialization

--- VideoTagger/src/attributes/core/simple_attribute_instance.hpp
+++ VideoTagger/src/attributes/core/simple_attribute_instance.hpp
@@ -44,8 +44,7 @@ namespace vt
 
 		[[nodiscard]] virtual nlohmann::ordered_json serialize() const override
 		{
-			//nlohmann::ordered_json json = impl::attribute_ref<simple_attribute<type>>::serialize();
-			nlohmann::ordered_json json;
+			nlohmann::ordered_json json = impl::attribute_ref<simple_attribute<type>>::serialize();
 			json["value"] = value();
 			return json;
 		}
