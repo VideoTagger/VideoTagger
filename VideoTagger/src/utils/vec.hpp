bb50b91e5ea33d2d43cb1149824aa7640fe46c7f
Vexte <60191743+Vexte@users.noreply.github.com>
2026-05-04 17:35:04 +0200

    Added utils::vec arithmetic operators

--- VideoTagger/src/utils/vec.hpp
+++ VideoTagger/src/utils/vec.hpp
@@ -124,6 +124,72 @@ namespace vt::utils
 		}
 	};
 
+	template<typename type, size_t dims>
+	inline constexpr vec<type, dims> operator+(const vec<type, dims>& left, const vec<type, dims>& right)
+	{
+		vec<type, dims> result;
+		for (size_t i = 0; i < dims; i++)
+		{
+			result[i] = left[i] + right[i];
+		}
+		return result;
+	}
+
+	template<typename type, size_t dims>
+	inline constexpr vec<type, dims> operator-(const vec<type, dims>& left, const vec<type, dims>& right)
+	{
+		vec<type, dims> result;
+		for (size_t i = 0; i < dims; i++)
+		{
+			result[i] = left[i] - right[i];
+		}
+		return result;
+	}
+
+	template<typename type, size_t dims>
+	inline constexpr vec<type, dims> operator*(const vec<type, dims>& left, const vec<type, dims>& right)
+	{
+		vec<type, dims> result;
+		for (size_t i = 0; i < dims; i++)
+		{
+			result[i] = left[i] * right[i];
+		}
+		return result;
+	}
+
+	template<typename type, size_t dims>
+	inline constexpr vec<type, dims> operator/(const vec<type, dims>& left, const vec<type, dims>& right)
+	{
+		vec<type, dims> result;
+		for (size_t i = 0; i < dims; i++)
+		{
+			result[i] = left[i] / right[i];
+		}
+		return result;
+	}
+
+	template<typename vec_type, size_t dims, typename value_type>
+	inline constexpr vec<vec_type, dims> operator*(const vec<vec_type, dims>& vector, value_type value)
+	{
+		vec<vec_type, dims> result;
+		for (size_t i = 0; i < dims; i++)
+		{
+			result[i] = vector[i] * value;
+		}
+		return result;
+	}
+
+	template<typename vec_type, size_t dims, typename value_type>
+	inline constexpr vec<vec_type, dims> operator/(const vec<vec_type, dims>& vector, value_type value)
+	{
+		vec<vec_type, dims> result;
+		for (size_t i = 0; i < dims; i++)
+		{
+			result[i] = vector[i] / value;
+		}
+		return result;
+	}
+
 	template<typename type>
 	using vec2 = vec<type, 2>;
 
