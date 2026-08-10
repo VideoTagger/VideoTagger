51763c5330b24d6cfa1e0f58a6cb2c0cdd7ded57
ScriptExec <scriptexec.contact@gmail.com>
2026-08-10 21:00:37 +0200

    Added point support to SAM2 wand extension

--- VideoTagger/src/attributes/tools/extensions/wand_sam2_extension.cpp
+++ VideoTagger/src/attributes/tools/extensions/wand_sam2_extension.cpp
@@ -8,7 +8,7 @@
 
 namespace vt::ui
 {
-	wand_sam2_extension::wand_sam2_extension(const std::string& name) : impl::wand_tool_extension{ name }, mode_{ wand_sam2_mode::rectangle }
+	wand_sam2_extension::wand_sam2_extension(const std::string& name) : impl::wand_tool_extension{ name }, mode_{ wand_sam2_mode::rectangle }, is_fg_point_{ true }
 	{
 
 	}
@@ -26,6 +26,67 @@ namespace vt::ui
 	bool wand_sam2_extension::is_mask_mode() const
 	{
 		return mode_ == wand_sam2_mode::mask;
+	}
+
+	void wand_sam2_extension::handle_point_selection(video_id_t video_id, ImRect draw_rect, const utils::vec2<int>& tex_size)
+	{
+		static auto to_texture_space = [](const ImVec2& screen_pos, ImRect draw_rect, const utils::vec2<int>& tex_size) -> utils::vec2<int>
+		{
+			return math::scale_vec2(screen_pos, draw_rect.Min, draw_rect.Max, utils::vec2<int>{}, tex_size, false);
+		};
+
+		auto& io = ImGui::GetIO();
+
+		bool is_hovered = ImGui::IsWindowHovered();
+		bool is_focused = ImGui::IsWindowFocused();
+
+		bool is_positive = !io.KeyAlt;
+		bool is_mouse_clicked = ImGui::IsMouseClicked(ImGuiMouseButton_Left);
+		bool is_ctrl_held = io.KeyCtrl;
+		auto mouse_pos = to_texture_space(ImGui::GetMousePos(), draw_rect, tex_size);
+
+		const auto remove_point_brush_size = 10.f;
+
+		if (is_hovered and is_mouse_clicked)
+		{
+			bool needs_update = false;
+			if (is_ctrl_held)
+			{
+				for (auto& current_points_shape : { &foreground_points_, &background_points_ })
+				{
+					auto closest_point = current_points_shape->closest_point(mouse_pos, remove_point_brush_size);
+					if (closest_point != nullptr)
+					{
+						ptrdiff_t idx = closest_point - current_points_shape->points.data();
+						current_points_shape->points.erase(current_points_shape->points.begin() + idx);
+						needs_update = true;
+						break;
+					}
+				}
+			}
+			else
+			{
+				needs_update = true;
+				(is_positive ? foreground_points_ : background_points_).points.push_back(mouse_pos);
+			}
+
+			if (needs_update)
+			{
+				generate_mask(video_id, tex_size);
+			}
+		}
+		
+		if (is_focused)
+		{
+			foreground_points_.render_points(3.f, tex_size, draw_rect, IM_COL32(0, 255, 0, 255), IM_COL32(0, 255, 0, 127));
+			background_points_.render_points(3.f, tex_size, draw_rect, IM_COL32(255, 0, 0, 255), IM_COL32(255, 0, 0, 127));
+		}
+
+		if (is_hovered and is_ctrl_held)
+		{
+			auto zoom_factor = draw_rect.GetWidth() / (float)tex_size.x();
+			draw_remove_point_preview(ImGui::GetMousePos(), zoom_factor * remove_point_brush_size);
+		}
 	}
 
 	void wand_sam2_extension::generate_mask(video_id_t video_id, const utils::vec2<int>& tex_size, const std::optional<utils::vec4<float>>& rect)
@@ -101,13 +162,113 @@ namespace vt::ui
 				});
 			}
 			break;
+			case wand_sam2_mode::points:
+			{
+				if (foreground_points_.points.empty() and background_points_.points.empty())
+				{
+					auto mask_data = data();
+					if (mask_data != nullptr)
+					{
+						mask_data->mask.clear();
+						mask_data->recalculate_bounding_box();
+					}
+					return;
+				}
+
+				ctx_.tasks.run([this, rect = rect, &vid_data]()
+				{
+					set_busy(true);
+
+					auto& stream = vid_data.video;
+					image<image_pixel_format::rgb8> img({ stream.width(), stream.height() });
+					if (stream.update_from_current_frame(img))
+					{
+						try
+						{
+							auto sam = ctx_.model_registry.get_model<sam2_model>();
+							if (sam == nullptr or !sam->load_if_needed())
+							{
+								throw std::runtime_error("Failed to load model");
+							}
+							auto load_guard = model_load_guard{ sam };
+
+							auto encoder = sam->encoder();
+							if (encoder == nullptr) throw std::runtime_error("Encoder is null");
+							auto decoder = sam->decoder();
+							if (decoder == nullptr) throw std::runtime_error("Decoder is null");
+
+							auto res = encoder->encode(img);
+
+							sam2_decoder_prompt prompt;
+							for (auto& point : foreground_points_.points)
+							{
+								sam2_decoder_prompt_point prompt_point;
+								prompt_point.label = 1;
+								prompt_point.point = { static_cast<float>(point.x()), static_cast<float>(point.y()) };
+								prompt.points.push_back(prompt_point);
+							}
+							for (auto& point : background_points_.points)
+							{
+								sam2_decoder_prompt_point prompt_point;
+								prompt_point.label = 0;
+								prompt_point.point = { static_cast<float>(point.x()), static_cast<float>(point.y()) };
+								prompt.points.push_back(prompt_point);
+							}
+							auto dec_res = decoder->decode(res, prompt);
+
+							cv::Mat result_mask = dec_res.masks[0];
+							load_guard.release();
+
+							auto mask_data = data();
+							if (mask_data == nullptr)
+							{
+								debug::error("Mask data is null, while generating mask");
+							}
+							else
+							{
+								mask_data->mask.allocate(result_mask.cols, result_mask.rows);
+								auto cv_mask_data = image_to_cvmat(mask_data->mask);
+								result_mask.copyTo(cv_mask_data);
+								//ctx_.tasks.run_on_main([this, result_mask]()
+								//{
+								//	cv::imshow("SAM2 Mask", result_mask);
+								//});
+							}
+						}
+						catch (const std::exception& e)
+						{
+							debug::error("SAM2 encoder/decoder error: {}", e.what());
+						}
+					}
+					set_busy(false);
+				})
+				.then(ctx_.tasks.on_main(), [this]()
+				{
+					rect_select_tool::reset();
+				});
+			}
+			break;
 		}
+	}
+
+	void wand_sam2_extension::draw_remove_point_preview(const ImVec2& center, float brush_size)
+	{
+		auto* draw_list = ImGui::GetWindowDrawList();
+		auto outline_color = ctx_.current_theme.get_rgba(theme_color::tool_preview_outline);
+
+		auto half_brush_size = brush_size / 2.f;
+		draw_list->AddLine({ center.x - half_brush_size, center.y - half_brush_size }, { center.x + half_brush_size, center.y + half_brush_size }, IM_COL32(255, 0, 0, 255), 1.f);
+		draw_list->AddLine({ center.x - half_brush_size, center.y + half_brush_size }, { center.x + half_brush_size, center.y - half_brush_size }, IM_COL32(255, 0, 0, 255), 1.f);
 	}
 
 	void wand_sam2_extension::reset()
 	{
 		mode_ = wand_sam2_mode::rectangle;
 		rect_select_tool::reset();
+		foreground_points_.points.clear();
+		background_points_.points.clear();
+		is_fg_point_ = true;
+		set_data(nullptr);
 	}
 
 	uint32_t wand_sam2_extension::property_column_count() const
@@ -133,8 +294,13 @@ namespace vt::ui
 			case wand_sam2_mode::rectangle:
 			{
 				handle_rect_selection(video_id, ImRect(pos, pos + size), { static_cast<int>(tex_size.x), static_cast<int>(tex_size.y) });
-				break;
 			}
+			break;
+			case wand_sam2_mode::points:
+			{
+				handle_point_selection(video_id, ImRect(pos, pos + size), { static_cast<int>(tex_size.x), static_cast<int>(tex_size.y) });
+			}
+			break;
 			case wand_sam2_mode::mask:
 			{
 				auto shape_data = data();
@@ -145,6 +311,7 @@ namespace vt::ui
 				auto zoom_factor = size.x / tex_size.x;
 				draw_brush_preview(ImGui::GetMousePos(), zoom_factor * brush_size());
 			}
+			break;
 		}
 	}
 
@@ -177,6 +344,11 @@ namespace vt::ui
 			brush_tool::render_properties();
 		}
 		wand_tool_extension::render_properties();
+	}
+
+	void wand_sam2_extension::on_done()
+	{
+
 	}
 
 	void wand_sam2_extension::on_finish_selection(video_id_t video_id, const rectangle_shape& rect, const utils::vec2<int>& tex_size)
--- VideoTagger/src/attributes/tools/extensions/wand_sam2_extension.hpp
+++ VideoTagger/src/attributes/tools/extensions/wand_sam2_extension.hpp
@@ -7,6 +7,7 @@
 #include <attributes/shapes/rectangle_shape.hpp>
 #include <attributes/tools/extensions/impl/wand_tool_extension.hpp>
 #include <attributes/tools/impl/rect_select_tool.hpp>
+#include <attributes/shapes/points_shape.hpp>
 
 namespace vt::ui
 {
@@ -24,13 +25,19 @@ namespace vt::ui
 
 	private:
 		wand_sam2_mode mode_;
+		points_shape foreground_points_;
+		points_shape background_points_;
+		bool is_fg_point_;
 
 	public:
 		bool is_rect_mode() const;
 		bool is_points_mode() const;
 		bool is_mask_mode() const;
 
+		void handle_point_selection(video_id_t video_id, ImRect draw_rect, const utils::vec2<int>& tex_size);
+
 		void generate_mask(video_id_t video_id, const utils::vec2<int>& tex_size, const std::optional<utils::vec4<float>>& rect = std::nullopt);
+		void draw_remove_point_preview(const ImVec2& center, float brush_size);
 
 		virtual void reset() override;
 
@@ -39,6 +46,7 @@ namespace vt::ui
 		virtual void render_overlay(video_id_t video_id, ImVec2 pos, ImVec2 size, ImVec2 tex_size) override;
 		virtual void render_properties() override;
 
+		virtual void on_done() override;
 		void on_finish_selection(video_id_t video_id, const rectangle_shape& rect, const utils::vec2<int>& tex_size) override;
 	};
 }
